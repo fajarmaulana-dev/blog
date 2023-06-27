f57de58ad6ba39f8fa9a27d27deb7887d25f0eb4
felkira <fajarmaulana_math@students.unnes.ac.id>
2023-06-27 08:38:36 +0700

    not use cdn for font

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import {createApp} from 'vue';
+import {createApp} from '@vue/runtime-dom';
 import App from './App.vue';
 import router from './router';
 import 'aos/dist/aos.css';
 import './style.css';
+import './awesome.css';
+import '@fontsource/be-vietnam-pro';
+
 const app = createApp(App);
-
 app.use(router);
-
 app.mount('#app');
