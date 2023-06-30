48978159d46422846ec6ccea4e7cfd7d4a38d16a
felkira <fajarmaulana_math@students.unnes.ac.id>
2023-06-30 08:11:34 +0700

    edit responsibility

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -6,7 +6,7 @@ import Menu from '@/components/Menu.vue';
     <Menu />
     <div class="min-h-screen grid place-items-center">
         <div class="w-[80vw] max-w-[35rem] grid place-items-center">
-            <img src="@/assets/soon.avif" width="460" height="360" class="max-h-[50vh]" alt="Coming Soon Thumbnail" />
+            <img src="@/assets/soon.avif" class="max-h-[50vh]" alt="Coming Soon Thumbnail" />
         </div>
     </div>
 </template>
