1d3d86f2731627347b46420276db5c39d293fa6b
felkira <fajarmaulana_math@students.unnes.ac.id>
2023-07-01 21:06:37 +0700

    remove mobile highlight

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import router from './router';
 import 'aos/dist/aos.css';
 import './style.css';
 import './awesome.css';
+import {useRegisterSW} from 'virtual:pwa-register/vue';
+useRegisterSW({immediate: true});
 
 const app = createApp(App);
 app.use(router);
